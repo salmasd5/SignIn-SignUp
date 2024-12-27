52fed2bd737334f9cd01c994ad9d29a6e796c0f0
Salma Daigham <121318601+salmasd5@users.noreply.github.com>
2024-12-27 23:28:43 +0100

    Update

--- eslint.config.js
+++ eslint.config.js
@@ -10,6 +10,7 @@ export default [
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
+      globals: globals.browser,
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
